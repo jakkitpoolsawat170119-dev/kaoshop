b7dcb24f896d56cc852c1411277d13ac8e20e7cf
jakkitpoolsawat170119-dev <jakkitpoolsawat170119@gmail.com>
2026-05-18 16:33:28 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -106,7 +106,7 @@ export default async function Header() {
             </form>
           </div>
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-5">
             <DarkModeToggle />
             <Link
               href="/admin"
