7791238b29246ec412e8eb3f90484cd3b78ce1ba
jakkitpoolsawat170119-dev <jakkitpoolsawat170119@gmail.com>
2026-05-18 17:16:14 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,8 +52,8 @@ export default async function Header() {
               <Image
                 src="/Screenshot 2569-05-14 at 10.54.17.png"
                 alt="KaoShop"
-                width={72}
-                height={72}
+                width={ุ60}
+                height={60}
                 className="rounded-full object-cover"
                 priority
               />
